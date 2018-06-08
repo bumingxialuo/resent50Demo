9aeed847528efa7aaa4fa382183889d4bf1d027f
胡亚东 <huyd@erongdu.com>
2018-06-08 11:30:38 +0800

    Initial Commit

---
+++ resent50Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  resent50Demo
+//
+//  Created by xia on 2018/6/8.
+//  Copyright © 2018年 xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ resent50DemoTests/resent50DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  resent50DemoTests.m
+//  resent50DemoTests
+//
+//  Created by xia on 2018/6/8.
+//  Copyright © 2018年 xia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface resent50DemoTests : XCTestCase
+
+@end
+
+@implementation resent50DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
